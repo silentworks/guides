612e0dc880f69091890edec69b7ba1a44aaea3f3
Mark Parson <mark@delorum.com>
2011-03-24 14:20:43 -0600

    added guide partial (layout)

---
+++ templates/shared/_dropdown.php
@@ -0,0 +1,80 @@
+<script>
+//<![CDATA[
+	$(document).ready(function(){
+		$(".zoom").colorbox({width:"50%", inline:true, href:".overflow"});
+	});
+	
+	$(".drop-down").click(function () {
+   		$(".link-box").show();
+   	});
+
+   	$('.link-box').mouseleave(function () {
+   		$(".link-box").hide();
+   	});
+	
+	$(".close").click(function () {
+		$(".link-box").hide();
+		return false;
+   	});
+//]]>
+</script>
+
+<span class="guides-sprite drop-down">
+	Guides Index
+	<div class="link-box" style="display:none;">
+		<span class="guides-sprite top"></span>
+		<a href="#link" class="guides-sprite close"></a>
+		<h3>Guides Index</h3>
+		<ul class="left">
+			<li>Start Here</li>
+			<li><a href="#link">Getting Started with Pagoda Box</a></li>
+		</ul>
+		<ul class="right">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<ul class="left">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<ul class="right">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<ul class="left">
+			<li>Start Here</li>
+			<li><a href="#link">Getting Started with Pagoda Box</a></li>
+		</ul>
+		<ul class="right">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<ul class="left">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<ul class="right">
+			<li>Environment Management</li>
+			<li><a href="#link">Page Title</a></li>
+			<li><a href="#link">Page Titles shouldn't extend to two lines</a></li>
+			<li><a href="#link">Can we build the dropdown like that?</a></li>
+			<li><a href="#link">This could be a medium one</a></li>
+		</ul>
+		<span class="guides-sprite bottom"></span>
+	</div>
+</span>
