97cb9acfb2f831bb0e8d61720b0ec6b337fc226c
Mark Parson <mark@delorum.com>
2011-03-28 12:18:34 -0600

    css tweaks for code and for li

--- templates/guides/getting-started/understanding-the-box-file.php
+++ templates/guides/getting-started/understanding-the-box-file.php
@@ -1,6 +1,22 @@
 {% extends "layouts/guide.php" %}
 
 {% block content %}
+<div class="right-col">
+	<div class="vertical-guides-sprite table-contents">
+		<span class="guides-sprite top-corner"></span>
+		<h3>Table of Contents</h3>
+		<h4>What's a .box File?</h4>
+		<ul>
+			<li><a href="#the-basics">The Basics</a></li>
+			<li><a href="#general-tips">General Tips</a></li>
+		</ul>
+		<h4><a href="#sample-files">Sample Files</a></h4>
+		<ul>
+			<li><span class="guides-sprite tag"></span><a href="#default-box-config-settings">Default .box Config Settings</a></li>
+		</ul>
+		<span class="guides-sprite bottom-corner"></span>
+	</div>
+</div>
 
 <div class="left-col">
 	<h1>Understanding the Box File</h1>
@@ -12,29 +28,29 @@
 		<li><span class="guides-sprite check"></span>And much more...</li>
 	</ul>
 	<div class="line-divider"></div>
-	<h2>What's a .box File?'</h2>
+	<h2>What's a .box File?</h2>
 	<div class="justify">
-		<h4 id="#-the-basics">The Basics</h4>
+		<h4 id="the-basics">The Basics</h4>
 		<p>Pagoda Box is an app deployment platform. Since the focus of the platform is your app, and not the services that support the platform, all configuration related to your app’s deployment is contained in one single file (.box), located at the base of your git repo. In short, the .box file will provide the necessary configuration to deploy your specific source code. The .box file is intended to be re-used whenever possible, especially when using a framework for multiple projects. When a .box file is not present, we will load a default .box file which might work for your app.</p>
 		<h3 class="tag"><span class="guides-sprite w-cap"></span><span class="horizontal-guides-sprite white">Pagodabox is a custom server (ie: it's not Apache)</span><span class="guides-sprite w-end-cap"></span></h3>
 		<div class="block yellow">
 			<p>Before explaining all the possible configurations, please note that our infrastructure does not use Apache, nor any other known product. Pagoda Box is a custom solution written from the bottom up for performance, predictability, scalability, and hot code reloads. For this reason we do not read .htaccess files or any other server specific config files. Don’t worry, since we wrote our own service, we also wrote the .box file spec. It’s so simple you’ll probably pee your pants.</p>
 		</div>
-		<h4 id="#-general-tips">General Tips</h4>
+		<h4 id="general-tips">General Tips</h4>
 		<ol>
-			<li>The .box file must be valid yaml markup. You can brush up on your yaml here: http://yaml.org/start.html or check your syntax here: http://instantyaml.appspot.com/.</li>
-			<li>It is only necessary to use a .box file when the defaults are insufficient.</li>
-			<li>You only need to specify the difference between the default configuration and your specific configuration. That means, if only one setting needs to be changed, create a .box file with only that one setting. No need to go overboard.</li>
-			<li>Since your app is tracked via git, so is your .box file. With each new deploy (including rollbacks), your .box configuration will be loaded and matched to the contents of your .box file at that commit point.</li>
-			<li>When your app is created, if you specify a framework, a custom tailored .box file will become the new default. At all times, we’ll provide a link to the .box file we are using as your default.</li>
+			<li><p>The .box file must be valid yaml markup. You can brush up on your yaml here: <a href="http://yaml.org/start.html" target="_blank">yaml.org/start.html</a> or check your syntax here: <a href="http://instantyaml.appspot.com/" target="_blank">instantyaml.appspot.com</a>.</p></li>
+			<li><p>It is only necessary to use a .box file when the defaults are insufficient.</p></li>
+			<li><p>You only need to specify the difference between the default configuration and your specific configuration. That means, if only one setting needs to be changed, create a .box file with only that one setting. No need to go overboard.</p></li>
+			<li><p>Since your app is tracked via git, so is your .box file. With each new deploy (including rollbacks), your .box configuration will be loaded and matched to the contents of your .box file at that commit point.</p></li>
+			<li><p>When your app is created, if you specify a framework, a custom tailored .box file will become the new default. At all times, we’ll provide a link to the .box file we are using as your default.</p></li>
 		</ol>
 	</div>
-	<h2>Sample Files</h2>
+	<h2 id="sample-files">Sample Files</h2>
 	<div class="justify">
-		<h4 id="#-all-the-options">All The Options</h4>
+		<h4 id="all-the-options">All The Options</h4>
 		<p>Now for the nitty gritty. The following commented code snippet shows the default .box setting</p>
 		<h3 class="tag"><span class="guides-sprite cap"></span><span class="horizontal-guides-sprite title">BOX</span><span class="horizontal-guides-sprite green">Default .box Config Settings</span><span class="guides-sprite green-end-cap"></span></h3>
-		<div class="block grey code">
+		<div class="block grey code" id="default-box-config-settings">
 			<script class='brush: yaml' type='syntaxhighlighter'>
 				<![CDATA[
 					general:
@@ -102,10 +118,10 @@
 					    # check this link: /link/to/extensions
 					    extensions: [eaccelerator, mysql]
 
-					    # http://www.php.net/manual/en/errorfunc.configuration.php#ini.error-reporting
+					    # http://bit.ly/hE3GT7
 					    error_reporting: 'E_ALL'
 
-					    # http://www.php.net/manual/en/errorfunc.configuration.php#ini.display-errors
+					    # http://bit.ly/elKiAH
 					    display_errors: On
 
 					    # http://php.net/manual/en/ini.core.php
@@ -144,17 +160,6 @@
 </div>
 
 
-<div class="right-col">
-	<div class="vertical-guides-sprite table-contents">
-		<span class="guides-sprite top-corner"></span>
-		<h3>Table of Contents</h3>
-		<h4>What's a .box File?</h4>
-		<ul>
-			<li><a href="#-the-basics">The Basics</a></li>
-			<li><a href="#-general-tips">General Tips</a></li>
-		</ul>
-		<span class="guides-sprite bottom-corner"></span>
-	</div>
-</div>
+
 
 {% endblock %}
