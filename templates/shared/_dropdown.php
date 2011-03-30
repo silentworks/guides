0d06089eadda3eb4c91e07b59b614ec28736258f
Mark Parson <mark@delorum.com>
2011-03-30 17:11:47 -0600

    Adding new guides, minor css tweaks

--- templates/shared/_dropdown.php
+++ templates/shared/_dropdown.php
@@ -15,40 +15,40 @@
 		<h3>Guides Index</h3>
 		<ul class="left">
 			<li>Start Here</li>
-			<li><a href="#link">Getting Started with Pagoda Box</a></li>
-			<li><a href="#link">What You Need to Know About Pagoda</a></li>
-			<li><a href="#link">Recommended Workflow</a></li>
-			<li><a href="#link">Getting Started</a></li>
+			<li><a href="#link"><i>Getting Started with Pagoda Box (Coming)</i></a></li>
+			<li><a href="/index.php/getting-started/what-you-need-to-know-about-pagoda-box">What You Need to Know About Pagoda</a></li>
+			<li><a href="/index.php/getting-started/recommended-workflow">Recommended Workflow</a></li>
+			<li><a href="/index.php/getting-started/understanding-the-box-file">Understanding the .box Config File</a></li>
 		</ul>
 		<ul class="right">
 			<li>PHP</li>
-			<li><a href="#link">Sending Mail</a></li>
-			<li><a href="#link">Understanding the Environment</a></li>
-			<li><a href="#link">Connecting to Services</a></li>
-			<li><a href="#link">Configuration & Deployment</a></li>
+			<li><a href="/index.php/php/sending-mail-from-your-app">Sending Mail From Your App</a></li>
+			<li><a href="#link"><i>Understanding the Environment</i></a></li>
+			<li><a href="/index.php/php/connecting-to-services">Connecting to Services</a></li>
+			<li><a href="/index.php/php/configuration-and-deployment">Configuration & Deployment</a></li>
 		</ul>
 		<ul class="left">
 			<li>Processes</li>
-			<li><a href="#link">Getting Started</a></li>
-			<li><a href="#link">Creating a Database</a></li>			
-			<li><a href="#link">Going Live</a></li>
-			<li><a href="#link">Scaling Your App</a></li>
-			<li><a href="#link">Transferring Ownership</a></li>
-			<li><a href="#link">Creating a DNS Alias</a></li>
-			<li><a href="#link">Adding SSL</a></li>
-			<li><a href="#link">Adding Collaborators</a></li>
+			<li><a href="#link"><i>Getting Started</i></a></li>
+			<li><a href="#link"><i>Creating a Database</a</i>></li>			
+			<li><a href="#link"><i>Going Live</i></a></li>
+			<li><a href="#link"><i>Scaling Your App</i></a></li>
+			<li><a href="#link"><i>Transferring Ownership</i></a></li>
+			<li><a href="#link"><i>Creating a DNS Alias</i></a></li>
+			<li><a href="#link"><i>Adding SSL</i></a></li>
+			<li><a href="#link"><i>Adding Collaborators</i></a></li>
 		</ul>
 		<ul class="right">
 			<li>Database</li>
-			<li><a href="#link">MySQL</a></li>
+			<li><a href="#link"><i>MySQL</i></a></li>
 		</ul>
 		<ul class="right">
 			<li>Frameworks</li>
-			<li><a href="#link">Cake</a></li>
-			<li><a href="#link">Drupal</a></li>
-			<li><a href="#link">Magento</a></li>
-			<li><a href="#link">Symfony</a></li>
-			<li><a href="#link">Wordpress</a></li>
+			<li><a href="#link"><i>Cake</i></a></li>
+			<li><a href="#link"><i>Drupal</i></a></li>
+			<li><a href="#link"><i>Magento</i></a></li>
+			<li><a href="#link"><i>Symfony</i></a></li>
+			<li><a href="#link"><i>Wordpress</i></a></li>
 		</ul>
 		<span class="guides-sprite bottom"></span>
 	</div>
