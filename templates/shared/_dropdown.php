a03a01e0fcb7802889d51fba8e20d7651fb98a5c
Mark Parson <mark@delorum.com>
2011-04-04 14:38:22 -0600

    fixed column issue on index

--- templates/shared/_dropdown.php
+++ templates/shared/_dropdown.php
@@ -13,23 +13,6 @@
 		<span class="guides-sprite top"></span>
 		<a href="#link" class="guides-sprite close"></a>
 		<h3>Guides Index</h3>
-		<ul class="left">
-			<li>Start Here</li>
-			<li><a href="#link"><i>Getting Started with Pagoda Box (Coming)</i></a></li>
-			<li><a href="/getting-started/launching-your-first-app">Launching Your First App</a></li>
-			<li><a href="/getting-started/what-you-need-to-know-about-pagoda-box">What You Need to Know About Pagoda</a></li>
-			<li><a href="/getting-started/recommended-workflow">Recommended Workflow</a></li>
-			<li><a href="/getting-started/understanding-the-box-file">Understanding the .box Config File</a></li>
-		</ul>
-		<ul class="left">
-			<li>Processes</li>
-			<li><a href="/processes/adding-a-collaborator">Adding Collaborators + Transferring Ownership</a></li>
-			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
-			<!-- <li><a href="#link"><i>Going Live (Coming)</i></a></li> -->
-			<li><a href="/processes/creating-a-dns-alias">Creating a DNS Alias</a></li>
-			<li><a href="#link"><i>Adding SSL (Coming)</i></a></li>
-			<li><a href="#link"><i>Scaling Your App (Coming)</i></a></li>
-		</ul>
 		<ul class="right">
 			<li>PHP</li>
 			<li><a href="/php/sending-mail-from-your-app">Sending Mail From Your App</a></li>
@@ -48,6 +31,23 @@
 			<li><a href="#link"><i>Magento (Coming)</i></a></li>
 			<li><a href="#link"><i>Symfony (Coming)</i></a></li>
 			<li><a href="/frameworks/wordpress">Wordpress</a></li>
+		</ul>
+		<ul class="left">
+			<li>Start Here</li>
+			<li><a href="#link"><i>Getting Started with Pagoda Box (Coming)</i></a></li>
+			<li><a href="/getting-started/launching-your-first-app">Launching Your First App</a></li>
+			<li><a href="/getting-started/what-you-need-to-know-about-pagoda-box">What You Need to Know About Pagoda</a></li>
+			<li><a href="/getting-started/recommended-workflow">Recommended Workflow</a></li>
+			<li><a href="/getting-started/understanding-the-box-file">Understanding the .box Config File</a></li>
+		</ul>
+		<ul class="left">
+			<li>Processes</li>
+			<li><a href="/processes/adding-a-collaborator">Adding Collaborators + Transferring Ownership</a></li>
+			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
+			<!-- <li><a href="#link"><i>Going Live (Coming)</i></a></li> -->
+			<li><a href="/processes/creating-a-dns-alias">Creating a DNS Alias</a></li>
+			<li><a href="#link"><i>Adding SSL (Coming)</i></a></li>
+			<li><a href="#link"><i>Scaling Your App (Coming)</i></a></li>
 		</ul>
 		<span class="guides-sprite bottom"></span>
 	</div>
