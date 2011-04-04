f6447716afa2ce37950391148394425d306a27b3
Mark Parson <mark@delorum.com>
2011-04-04 15:44:41 -0600

    removed italics

--- templates/shared/_dropdown.php
+++ templates/shared/_dropdown.php
@@ -16,25 +16,25 @@
 		<ul class="right">
 			<li>PHP</li>
 			<li><a href="/php/sending-mail-from-your-app">Sending Mail From Your App</a></li>
-			<li><a href="#link" class="coming">Understanding the Environment (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Understanding the Environment (Coming)</a></li>
 			<li><a href="/php/connecting-to-services">Connecting to Services</a></li>
-		</ul>s
+		</ul>
 		<ul class="right">
 			<li>Database</li>
 			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
-			<li><a href="#link" class="coming">MySQL (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">MySQL (Coming)</a></li>
 		</ul>
 		<ul class="right">
 			<li>Frameworks</li>
-			<li><a href="#link" class="coming">Cake (Coming)</a></li>
-			<li><a href="#link" class="coming">Drupal (Coming)</a></li>
-			<li><a href="#link" class="coming">Magento (Coming)</a></li>
-			<li><a href="#link" class="coming">Symfony (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Cake (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Drupal (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Magento (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Symfony (Coming)</a></li>
 			<li><a href="/frameworks/wordpress">Wordpress</a></li>
 		</ul>
 		<ul class="left">
 			<li>Start Here</li>
-			<li><a href="#link" class="coming">Getting Started with Pagoda Box (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Getting Started with Pagoda Box (Coming)</a></li>
 			<li><a href="/getting-started/launching-your-first-app">Launching Your First App</a></li>
 			<li><a href="/getting-started/what-you-need-to-know-about-pagoda-box">What You Need to Know About Pagoda</a></li>
 			<li><a href="/getting-started/recommended-workflow">Recommended Workflow</a></li>
@@ -44,10 +44,10 @@
 			<li>Processes</li>
 			<li><a href="/processes/adding-a-collaborator">Adding Collaborators + Transferring Ownership</a></li>
 			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
-			<!-- <li><a href="#link" class="coming">Going Live (Coming)</a></li> -->
+			<!-- <li><a href="#link" class="coming-soon">Going Live (Coming)</a></li> -->
 			<li><a href="/processes/creating-a-dns-alias">Creating a DNS Alias</a></li>
-			<li><a href="#link" class="coming">Adding SSL (Coming)</a></li>
-			<li><a href="#link" class="coming">Scaling Your App (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Adding SSL (Coming)</a></li>
+			<li><a href="#link" class="coming-soon">Scaling Your App (Coming)</a></li>
 		</ul>
 		<span class="guides-sprite bottom"></span>
 	</div>
