168d91919a3d0b246c6be5ebade127610f45d270
Mark Parson <mark@delorum.com>
2011-04-04 15:37:53 -0600

    removed i tags

--- templates/shared/_dropdown.php
+++ templates/shared/_dropdown.php
@@ -16,25 +16,25 @@
 		<ul class="right">
 			<li>PHP</li>
 			<li><a href="/php/sending-mail-from-your-app">Sending Mail From Your App</a></li>
-			<li><a href="#link"><i>Understanding the Environment (Coming)</i></a></li>
+			<li><a href="#link" class="coming">Understanding the Environment (Coming)</a></li>
 			<li><a href="/php/connecting-to-services">Connecting to Services</a></li>
-		</ul>
+		</ul>s
 		<ul class="right">
 			<li>Database</li>
 			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
-			<li><a href="#link"><i>MySQL (Coming)</i></a></li>
+			<li><a href="#link" class="coming">MySQL (Coming)</a></li>
 		</ul>
 		<ul class="right">
 			<li>Frameworks</li>
-			<li><a href="#link"><i>Cake (Coming)</i></a></li>
-			<li><a href="#link"><i>Drupal (Coming)</i></a></li>
-			<li><a href="#link"><i>Magento (Coming)</i></a></li>
-			<li><a href="#link"><i>Symfony (Coming)</i></a></li>
+			<li><a href="#link" class="coming">Cake (Coming)</a></li>
+			<li><a href="#link" class="coming">Drupal (Coming)</a></li>
+			<li><a href="#link" class="coming">Magento (Coming)</a></li>
+			<li><a href="#link" class="coming">Symfony (Coming)</a></li>
 			<li><a href="/frameworks/wordpress">Wordpress</a></li>
 		</ul>
 		<ul class="left">
 			<li>Start Here</li>
-			<li><a href="#link"><i>Getting Started with Pagoda Box (Coming)</i></a></li>
+			<li><a href="#link" class="coming">Getting Started with Pagoda Box (Coming)</a></li>
 			<li><a href="/getting-started/launching-your-first-app">Launching Your First App</a></li>
 			<li><a href="/getting-started/what-you-need-to-know-about-pagoda-box">What You Need to Know About Pagoda</a></li>
 			<li><a href="/getting-started/recommended-workflow">Recommended Workflow</a></li>
@@ -44,10 +44,10 @@
 			<li>Processes</li>
 			<li><a href="/processes/adding-a-collaborator">Adding Collaborators + Transferring Ownership</a></li>
 			<li><a href="/processes/creating-a-database">Creating a Database</a></li>			
-			<!-- <li><a href="#link"><i>Going Live (Coming)</i></a></li> -->
+			<!-- <li><a href="#link" class="coming">Going Live (Coming)</a></li> -->
 			<li><a href="/processes/creating-a-dns-alias">Creating a DNS Alias</a></li>
-			<li><a href="#link"><i>Adding SSL (Coming)</i></a></li>
-			<li><a href="#link"><i>Scaling Your App (Coming)</i></a></li>
+			<li><a href="#link" class="coming">Adding SSL (Coming)</a></li>
+			<li><a href="#link" class="coming">Scaling Your App (Coming)</a></li>
 		</ul>
 		<span class="guides-sprite bottom"></span>
 	</div>
