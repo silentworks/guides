87a5fd67f828ba8ee473ad25cba7771c7678d31f
Mark Parson <mark@delorum.com>
2011-04-11 14:15:33 -0600

    Just cleaning up Tyler's bad language ;-)

--- templates/guides/getting-started/recommended-workflow.php
+++ templates/guides/getting-started/recommended-workflow.php
@@ -25,7 +25,7 @@
 	<div class="line-divider"></div>
 	<h2 id="-code-it">Code It</h2>
 	<div class="justify">
-		<p>Do what you do best–code. Let's be honest, you are a developer, and development is cool. The thought of configuring a firewall, router, fail-over environment, and general package installation—let’s face it—isn’t that cool. Sure, you've got your Linux merit badge and you're pretty good at it, but that isn't what makes you tick. Focus on your code. Add revisions and features, then collaborate with your developer buddies. Re-format the LAMP configuration sector of your brain and re-fill it with coding awesomeness.</p>
+		<p>Do what you do best–code. Let's be honest, you are a developer, and development is cool. The thought of configuring a firewall, router, fail-over environment, and general package installation—let’s face it—isn’t that cool. Sure, you've got your Linux merit badge and you're pretty good at it, but it's not what makes you tick. Focus on code, add revisions and features, then collaborate with your developer buddies. Pagoda lets you reformat the LAMP configuration sector of your brain and re-fill it with coding goodness.</p>
 	</div>
 	<h2 id="-git">Git</h2>
 	<div class="justify">
@@ -37,7 +37,7 @@
 	</div>
 	<h2 id="-loadit">Load It</h2>
 	<div class="justify">
-		<p>Once you've committed the perfect revision, you can deploy that snapshot to all clones simultaneously with the click of a button in the admin panel, or by a simple console command. Within seconds your changes will be live. If by chance your code wasn't quite ready, feel free to rollback and watch your live app recover within seconds.</p>
+		<p>Once you've committed the perfect revision, deploy that snapshot to all clones simultaneously with the click of a button in the admin panel, or by a simple console command. Within seconds your changes will be live. If by chance your code wasn't quite ready, feel free to rollback (also one-click) and watch your live app recover within seconds.</p>
 	</div>
 </div>
 
--- templates/guides/getting-started/understanding-the-box-file.php
+++ templates/guides/getting-started/understanding-the-box-file.php
@@ -25,16 +25,16 @@
 		<li><span class="guides-sprite check"></span>Configure PHP for your app</li>
 		<li><span class="guides-sprite check"></span>Create URL rewrites</li>
 		<li><span class="guides-sprite check"></span>Create writable directories</li>
-		<li><span class="guides-sprite check"></span>And much more...</li>
+		<li><span class="guides-sprite check"></span>Mucho mucho more...</li>
 	</ul>
 	<div class="line-divider"></div>
 	<h2>What's a .box File?</h2>
 	<div class="justify">
 		<h4 id="the-basics">The Basics</h4>
-		<p>Pagoda Box is an app deployment platform. Since the focus of the platform is your app, and not the services that support the platform, all configuration related to your app’s deployment is contained in one single file (.box), located at the base of your git repo. In short, the .box file will provide the necessary configuration to deploy your specific source code. The .box file is intended to be re-used whenever possible, especially when using a framework for multiple projects. When a .box file is not present, we will load a default .box file which might work for your app.</p>
+		<p>Pagoda Box is an app deployment platform. Since the focus of the platform is your app, and not the services that support the platform, all configuration related to your app’s deployment is contained in a single file (.box), located at the base of your git repo. In short, the .box file specifies how Pagoda Box should be configured for your source code. The .box file is intended to be re-used whenever possible, especially when using frameworks for multiple projects. When a .box file is not present, we load a default .box file which works for most apps.</p>
 		<h3 class="tag"><span class="guides-sprite w-cap"></span><span class="horizontal-guides-sprite white">Pagoda Box is a custom server (ie: it's not Apache)</span><span class="guides-sprite w-end-cap"></span></h3>
 		<div class="block yellow">
-			<p>Before explaining all the possible configurations, please note that our infrastructure does not use Apache, nor any other known product. Pagoda Box is a custom solution written from the bottom up for performance, predictability, scalability, and hot code reloads. For this reason we do not read .htaccess files or any other server specific config files. Don’t worry, since we wrote our own service, we also wrote the .box file spec. It’s so simple you’ll probably pee your pants.</p>
+			<p>Before explaining all the possible configurations, please note that our infrastructure does not use Apache. Pagoda Box is a custom solution written from the bottom up for performance, predictability, scalability, and hot code reloads. For this reason we do not read .htaccess files or any other server specific config files. But no worries, we also wrote the .box file spec–It’s so simple you’ll probably pee your pants.</p>
 		</div>
 		<h4 id="general-tips">General Tips</h4>
 		<ol>
@@ -42,7 +42,7 @@
 			<li><p>It is only necessary to use a .box file when the defaults are insufficient.</p></li>
 			<li><p>You only need to specify the difference between the default configuration and your specific configuration. That means, if only one setting needs to be changed, create a .box file with only that one setting. No need to go overboard.</p></li>
 			<li><p>Since your app is tracked via git, so is your .box file. With each new deploy (including rollbacks), your .box configuration will be loaded and matched to the contents of your .box file at that commit point.</p></li>
-			<li><p>When your app is created, if you specify a framework, a custom tailored .box file will become the new default. At all times, we’ll provide a link to the .box file we are using as your default.</p></li>
+			<li><p>If you specify a framework when your app is created, a framework tailored .box file will become the new default. At all times, we provide a link to the .box file we are using as your default.</p></li>
 		</ol>
 	</div>
 	<h2 id="sample-files">Sample Files</h2>
@@ -53,100 +53,108 @@
 		<div class="block grey code" id="default-box-config-settings">
 			<script class='brush: yaml' type='syntaxhighlighter'>
 				<![CDATA[
-			    # network shared directories that every
-			    # node has access to in real time.
-			    # 
-			    # CAUTION: these should be used sparingly!
-			    # You should only use these for upload content
-			    # or possible as a central cache store.
-			    # 
-			    # WARNING: Once you specify a directory here,
-			    # deploys will only copy new files into these
-			    # directories. The file contents will no longer
-			    # be tracked via git on the Pagoda side.
-			    writable_directories: [/dirA, /dirB]
+					################################################
+					## WEB
+					################################################
+					
+					# network shared directories that every
+					# node has access to in real time.
+					# 
+					# CAUTION: these should be used sparingly!
+					# You should only use these for upload content
+					# or possible as a central cache store.
+					# 
+					# WARNING: Once you specify a directory here,
+					# deploys will only copy new files into these
+					# directories. The file contents will no longer
+					# be tracked via git on the Pagoda side.
+					writable_directories: [/dirA, /dirB]
 
-			    # The root of your executable app source.
-			    # For instance, if you like to house your app
-			    # in /app for security or organizational
-			    # purposes, you can specify that here.
-			    # All further configurations will be relative
-			    # to this directory. Default is the root of 
-			    # you project.
-			    document_root: /
+					# The root of your executable app source.
+					# For instance, if you like to house your app
+					# in /app for security or organizational
+					# purposes, you can specify that here.
+					# All further configurations will be relative
+					# to this directory. Default is the root of 
+					# you project.
+					document_root: /
 
-			    # When a path is not specified in the url
-			    # which file would you like to serve, 
-			    # in order from left to right until one
-			    # of the files exist in the requested
-			    # directory .
-			    index_list: [index.html, index.php]
+					# When a path is not specified in the url
+					# which file would you like to serve, 
+					# in order from left to right until one
+					# of the files exist in the requested
+					# directory .
+					index_list: [index.html, index.php]
 
-			    # When a url contains a path that is not
-			    # matched on the file system (ie: /product/13)
-			    # which file would you like us to push the
-			    # request through. This is often refered to 
-			    # as the bootfile or boilerplate.
-			    default_gateway: index.php
+					# When a url contains a path that is not
+					# matched on the file system (ie: /product/13)
+					# which file would you like us to push the
+					# request through. This is often refered to 
+					# as the bootfile or boilerplate.
+					default_gateway: index.php
 
-			    # Lyon what is this?
-			    locations: []
+					# Lyon what is this?
+					locations: []
 
-			    # When an error is encountered in the request
-			    # cycle, we can route the request to a different
-			    # path or url if you'd like. 
-			    # 
-			    # The format is as follows:
-			    # error_number: new_path
-			    error_pages: 
-			        404: /404.html
+					# When an error is encountered in the request
+					# cycle, we can route the request to a different
+					# path or url if you'd like. 
+					# 
+					# The format is as follows:
+					# error_number: new_path
+					error_pages:
+					    404: /404.html
 
+					
+					################################################
+					## PHP SPECIFIC SETTINGS
+					################################################
+					
+					# The version of php to load into your secure
+					# executable container. Defaults to newest.
+					php_version: 5.3.5
 
-			    # The version of php to load into your secure
-			    # executable container. Defaults to newest.
-			    version: 5.3.5
+					# By default, your php executable is extremely
+					# slim, and only contains the core interpreter.
+					# All additional extensions will be loaded when
+					# we spawn your executable processes. You can
+					# specify them simply as a comma delimited list
+					# To see a list of all available extensions 
+					# check this link: /link/to/extensions
+					php_extensions: [eaccelerator, mysql]
 
-			    # By default, your php executable is extremely
-			    # slim, and only contains the core interpreter.
-			    # All additional extensions will be loaded when
-			    # we spawn your executable processes. You can
-			    # specify them simply as a comma delimited list
-			    # To see a list of all available extensions 
-			    # check this link: /link/to/extensions
-			    extensions: [eaccelerator, mysql]
+					# http://bit.ly/hE3GT7
+					php_error_reporting: 'E_ALL'
 
-			    # http://bit.ly/hE3GT7
-			    error_reporting: 'E_ALL'
+					# http://bit.ly/elKiAH
+					php_display_errors: On
 
-			    # http://bit.ly/elKiAH
-			    display_errors: On
+					# http://php.net/manual/en/ini.core.php
+					php_short_open_tag: On
 
-			    # http://php.net/manual/en/ini.core.php
-			    short_open_tag: On
+					# http://php.net/manual/en/ini.core.php
+					php_default_mimetype: text/html
 
-			    # http://php.net/manual/en/ini.core.php
-			    default_mimetype: text/html
+					# http://php.net/manual/en/ini.core.php
+					php_date_timezone: US/central
 
-			    # http://php.net/manual/en/ini.core.php
-			    date_timezone: US/central
+					# http://php.net/manual/en/ini.core.php
+					php_max_execution_time: 30
 
-			    # http://php.net/manual/en/ini.core.php
-			    max_execution_time: 30
+					# http://php.net/manual/en/ini.core.php
+					php_max_input_time: 60
 
-			    # http://php.net/manual/en/ini.core.php
-			    max_input_time: 60
+					# http://php.net/manual/en/ini.core.php
+					php_post_max_size: 8M
 
-			    # http://php.net/manual/en/ini.core.php
-			    post_max_size: 8M
+					# http://php.net/manual/en/ini.core.php
+					php_file_uploads: On
 
-			    # http://php.net/manual/en/ini.core.php
-			    file_uploads: On
+					# http://php.net/manual/en/ini.core.php
+					php_upload_max_filesize: 2M
 
-			    # http://php.net/manual/en/ini.core.php
-			    upload_max_filesize: 2M
-
-			    # http://php.net/manual/en/ini.core.php
-			    max_file_uploads: 20
+					# http://php.net/manual/en/ini.core.php
+					php_max_file_uploads: 20
 				]]>
 			</script>
 			<div class="extra">
