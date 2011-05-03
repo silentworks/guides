3b50addcf90d3c0c603b50ec44c66970f74a46ea
cphillipp <clay@delorum.com>
2011-05-03 14:58:33 -0600

    updated guides header

--- templates/shared/_header.php
+++ templates/shared/_header.php
@@ -2,13 +2,13 @@
 <div class="background_for header"></div>
 <div class="header site">
 	<div class="wrapper logo"><a href="http://www.pagodabox.com" class="sprite logo beta"></a></div>
-	<a href="https://dashboard.pagodabox.com/apps" class="login">Dashboard</a>
-	<a href="https://dashboard.pagodabox.com/register" class="sprite create account">Create A Free Account</a>	
+	<a href="https://dashboard.pagodabox.com/apps" class="login">Login</a>
+	<a href="https://dashboard.pagodabox.com/register" class="sprite create account">Request An Invite</a>	
 	<ul class="nav">
 		<li><a href="http://www.pagodabox.com/features"><span class="sprite left"></span>Features<span class="sprite right"></span></a></li>
 		<li><a href="http://www.pagodabox.com/architecture"><span class="sprite left"></span>Architecture<span class="sprite right"></span></a></li>
 		<li><a href="http://www.pagodabox.com/pricing"><span class="sprite left"></span>Pricing<span class="sprite right"></span></a></li>
-		<!-- <li><a href="http://www.pagodabox.com/contact"><span class="sprite left"></span>Contact<span class="sprite right"></span></a></li> -->
+		<li><a href="http://blog.pagodabox.com">Blog</a></li>
 		<li class="last"><a href="http://guides.pagodabox.com" class="active"><span class="sprite left"></span>Guides<span class="sprite right"></span></a></li>
 	</ul>
 </div>
